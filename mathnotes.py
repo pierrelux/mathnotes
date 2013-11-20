2365f7ad9c71ffe35ec86b0067b2d32a6327fba0
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-11-19 22:16:45 -0500

    Add tag feature and display in client

--- mathnotes.py
+++ mathnotes.py
@@ -1,75 +1,125 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr Tests
-    ~~~~~~~~~~~~
+    Flaskr
+    ~~~~~~
 
-    Tests the Flaskr application.
+    A microblog example application written as Flask tutorial with
+    Flask and sqlite3.
 
     :copyright: (c) 2010 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-import os
-import flaskr
-import unittest
-import tempfile
+import markdown
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, Markup
 
 
-class FlaskrTestCase(unittest.TestCase):
+# create our little application :)
+app = Flask(__name__)
 
-    def setUp(self):
-        """Before each test, set up a blank database"""
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        flaskr.init_db()
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE='/tmp/flaskr.db',
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-    def tearDown(self):
-        """Get rid of the database again after each test."""
-        os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
-    def login(self, username, password):
-        return self.app.post('/login', data=dict(
-            username=username,
-            password=password
-        ), follow_redirects=True)
 
-    def logout(self):
-        return self.app.get('/logout', follow_redirects=True)
+def init_db():
+    """Creates the database tables."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
-    # testing functions
 
-    def test_empty_db(self):
-        """Start with a blank database."""
-        rv = self.app.get('/')
-        assert b'No entries here so far' in rv.data
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
-    def test_login_logout(self):
-        """Make sure login and logout works"""
-        rv = self.login(flaskr.app.config['USERNAME'],
-                        flaskr.app.config['PASSWORD'])
-        assert b'You were logged in' in rv.data
-        rv = self.logout()
-        assert b'You were logged out' in rv.data
-        rv = self.login(flaskr.app.config['USERNAME'] + 'x',
-                        flaskr.app.config['PASSWORD'])
-        assert b'Invalid username' in rv.data
-        rv = self.login(flaskr.app.config['USERNAME'],
-                        flaskr.app.config['PASSWORD'] + 'x')
-        assert b'Invalid password' in rv.data
 
-    def test_messages(self):
-        """Test that messages work"""
-        self.login(flaskr.app.config['USERNAME'],
-                   flaskr.app.config['PASSWORD'])
-        rv = self.app.post('/add', data=dict(
-            title='<Hello>',
-            text='<strong>HTML</strong> allowed here'
-        ), follow_redirects=True)
-        assert b'No entries here so far' not in rv.data
-        assert b'&lt;Hello&gt;' in rv.data
-        assert b'<strong>HTML</strong> allowed here' in rv.data
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.template_filter("markdown")
+def render_markdown(markdown_text):
+    return Markup(markdown.markdown(markdown_text))
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text, group_concat(name, " ") as tag from tags t, tagsmap tm, entries e where t.id = tm.tag_id and tm.note_id = e.id group by note_id, title, text order by note_id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+
+    cur = db.cursor()
+    cur.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    entrieid = cur.lastrowid
+
+    tagnames = request.form.getlist('tags')
+    for tag in tagnames:
+      cur.execute("""insert or ignore into tags (name) values (?)""", [tag])
+
+    placeholders  = ', '.join('?'*len(tagnames))
+    query = "select id from tags where name in (%s)" %placeholders
+    cur.execute(query, tagnames)
+    tagsid = cur.fetchall()
+    taglinks = [[entrieid] + [int(t[0])] for t in tagsid]
+
+    cur.executemany("insert into tagsmap(note_id, tag_id) values(?,?)", taglinks)
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    init_db()
+    app.run()
