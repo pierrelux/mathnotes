9da5f739b962d73d5fc4fc982eefddcccf85f6a1
pgayane <pgayane@gmail.com>
2013-11-29 20:05:55 -0500

    uptodate copy from upstream

--- mathnotes.py
+++ mathnotes.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE='/tmp/flaskr.db',
+    DATABASE='flaskr.db',
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
