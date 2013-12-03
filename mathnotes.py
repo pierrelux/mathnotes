4611a09647b2e67801b8553c0fc93f18f3968e42
pgayane <pgayane@gmail.com>
2013-12-03 12:40:00 -0500

    SQLalchemy added
    -everything changed to use SQLalchemy
    -database name changed
    -settings.py added which now contains both SQLalchemy and flask
    configuration

--- mathnotes.py
+++ mathnotes.py
@@ -9,50 +9,21 @@
     :copyright: (c) 2010 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-import markdown
+
 import requests
+import markdown
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, Markup, jsonify
 
+from settings import flaskconfig as config
+import dbmodels
+from dbmodels import Session, Note, Tag, Citation
 
 # create our little application :)
 app = Flask(__name__)
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE='flaskr.db',
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
+app.config.update(config)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def init_db():
-    """Creates the database tables."""
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
 
 @app.teardown_appcontext
 def close_db(error):
@@ -66,9 +37,7 @@ def render_markdown(markdown_text):
 
 @app.route('/')
 def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text, group_concat(name, " ") as tag from tags t, tagsmap tm, entries e where t.id = tm.tag_id and tm.note_id = e.id group by note_id, title, text order by note_id desc')
-    entries = cur.fetchall()
+    entries = dbmodels.getAll();
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/search/citation/<paper_title>')
@@ -91,44 +60,29 @@ def get_citation(paper_title):
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-
-    cur = db.cursor()
-    cur.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    entrieid = cur.lastrowid
-
-    tagnames = request.form.getlist('tags')
-    for tag in tagnames:
-      cur.execute("""insert or ignore into tags (name) values (?)""", [tag])
-
-    placeholders  = ', '.join('?'*len(tagnames))
-    query = "select id from tags where name in (%s)" %placeholders
-    cur.execute(query, tagnames)
-    tagsid = cur.fetchall()
-    taglinks = [[entrieid] + [int(t[0])] for t in tagsid]
-
-    cur.executemany("insert into tagsmap(note_id, tag_id) values(?,?)", taglinks)
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+	if not session.get('logged_in'):
+		abort(401)
+	note = Note(title = request.form['title'], text = request.form['text'], tagnames = request.form.getlist('tags'))
+	dbmodels.save_db(note)
+	
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+	print 'bla'
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
 
 
 @app.route('/logout')
@@ -139,5 +93,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    init_db()
-    app.run()
+	dbmodels.init_db()
+	app.run()
---
+++ settings.py
@@ -0,0 +1,10 @@
+dbconfig = {
+"url":"sqlite:///mathnotesDB.db",
+}
+
+flaskconfig ={
+"DEBUG":True,
+"SECRET_KEY":'development key',
+"USERNAME":'admin',
+"PASSWORD":'default'
+}
