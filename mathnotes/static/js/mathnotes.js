f4e2aee060748e2abb81ddfea93393a34f831ca7
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2014-01-04 03:46:38 -0500

    Add and remove references to editor

--- mathnotes/static/js/mathnotes.js
+++ mathnotes/static/js/mathnotes.js
@@ -7,69 +7,56 @@ $("#menu-toggle").click(function(e) {
     $("#sidebar-wrapper").toggleClass("active");
 });
 
-var Reference = $$({},
-  '<li><span data-bind="content"></span></li>',
-   {});
+var Reference = $$({
+  model: {},
+  view: { format: '<li class="list-group-item">\
+                   <button type="button" class="close pull-right" aria-hidden="true">&times;</button>\
+                   <h4 class="list-group-item-heading"><span data-bind="title"/></h4>\
+                   <span data-bind="authors"/></li>'
+  },
+  controller: {
+   'click button': function(){
+      this.destroy();
+    }
+  }
+});
 
-var ReferenceList = $$({},
-      '<div><h4>References</h4>\
-         <ul></ul>\
-         <input id="reference-search" type="text" class="typeahead borderless form-control" data-bind="content" placeholder="Search by title or author">\
-       </div>',
-  {
-  'keyup #reference-search': function(e){
-     var code = e.keyCode || e.which;
-     if (code == 13){
-       var bibRef = $$(Reference, {content: this.model.get('content')});
-       this.append(bibRef, "ul");
-     }
-   }
-  });
+var ReferenceList = $$({
+    model: {},
+    view: { format: '<ul class="list-group"></ul>' },
+    controller: {}
+});
 
-var NoteEditor = $$({},
-       '<div class="panel panel-default">\
-       <div class="panel-heading"><input type="text" class="form-control" name="title" placeholder="Title"></div>\
-       <div class="panel-body"><textarea name="text" class="form-control" rows="5">Write your stroke of thought</textarea>\
-       <span id="references"/></div>\
-	   <div class="panel-footer"><div class="btn-toolbar" role="toolbar">\
-         <div class="btn-group">\
-           <button type="button" class="btn btn-default"> <span class="glyphicon glyphicon-tags"></span></button>\
-           <button type="button" id="referenceBtn" class="btn btn-default">\
-             <span class="glyphicon glyphicon-link"></span>\
-           </button>\
-         </div>\
-         <div class="btn-group pull-right">\
-           <button type="submit" class="btn btn-primary">OK</button>\
-         </div>\
-       </div>\
-      </div></div>',
-   {
-     'click #referenceBtn': function() {
-        var refList = $$(ReferenceList, {});
-        this.append(refList, $('#references'));
-        $('#reference-search').typeahead({
-          name: 'accounts',
-          prefetch: {
-            url: '/references/hints/typeahead',
-            filter: function(resp) { return resp.items; }
-          },
-          remote: {
-            url: '/references/typeahead/%QUERY',
-            filter: function(resp) { return resp.items; }
-          },
-          template: [
-           '<p class="tt-suggestion-year">{{date}}</p>',
-           '<p class="tt-suggestion-title">{{value}}</p>',
-           '<p class="tt-suggestion-authors">{{authors}}</p>'
-          ].join(''),
-          engine: Hogan
-        });
-        $('#reference-search').on('typeahead:selected', function (object, datum) {
-           var bibRef = $$(Reference, {content: datum['value']});
-           refList.append(bibRef, "ul");
-         });
-      }
-   }
-);
+var NoteEditor = $$({
+  model: {},
+  view: { format: $('#noteEditor').html().trim() },
+  controller: {}
+});
 
 $$.document.append(NoteEditor, $('#noteEditor'))
+$('#referenceSearch').typeahead({
+    name: 'accounts',
+    prefetch: {
+    url: '/references/hints/typeahead',
+    filter: function(resp) { return resp.items; }
+    },
+    remote: {
+    url: '/references/typeahead/%QUERY',
+    filter: function(resp) { return resp.items; }
+    },
+    template: [
+    '<p class="tt-suggestion-year">{{date}}</p>',
+    '<p class="tt-suggestion-title">{{value}}</p>',
+    '<p class="tt-suggestion-authors">{{authors}}</p>'
+    ].join(''),
+    engine: Hogan
+});
+$$.document.append(NoteEditor, $('#content'));
+$$.document.after(ReferenceList, $('#textEditor'));
+$('#referenceSearch').on('typeahead:selected', function (object, datum) {
+    var ref = $$(Reference, {title: datum['title'], authors: datum['authors']});
+    ReferenceList.append(ref);
+    $('#referenceSearch').val('');
+});
+    var ref = $$(Reference, {title: 'asdfasdfas', authors: 'Authors'})
+    ReferenceList.append(ref);
